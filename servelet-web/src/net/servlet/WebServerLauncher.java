8a3a00d4884e30f2c5d5515b7146085daf319d98
leechulhan <dhfhfkwhdk@naver.com>
2022-09-13 23:09:42 +0900

    Embeded Tomcat 추가

---
+++ servelet-web/src/net/servlet/WebServerLauncher.java
@@ -0,0 +1,29 @@
+package net.servlet;
+
+import java.io.File;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+public class WebServerLauncher {
+	
+	// EmbedTomcat 설정 추가 
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "webapp/";
+        Tomcat tomcat = new Tomcat();
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+         
+        tomcat.setPort(Integer.valueOf(webPort));
+        Connector connector = tomcat.getConnector();
+        connector.setURIEncoding("UTF-8");
+        
+        // webapp/ 하위를 root 로 인식하도록 설정 
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+        tomcat.start();
+        tomcat.getServer().await(); 
+    }
+}
