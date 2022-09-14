7e77670ccff341bfaab2ff7d05ca5bc1c2fedf79
leechulhan <dhfhfkwhdk@naver.com>
2022-09-14 23:55:14 +0900

    Next 학습 Commit

---
+++ servelet-web/src/net/http/HelloWorldServlet.java
@@ -0,0 +1,29 @@
+package net.http;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.System.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+	
+	private static int testNum = 5;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res)
+		throws ServletException, IOException {
+		
+		System.out.println("HELLO SERVLET HELLO WORLD SERVLET !");
+		
+		PrintWriter out = res.getWriter();
+		out.print("Hello World !");
+	}
+	
+}
